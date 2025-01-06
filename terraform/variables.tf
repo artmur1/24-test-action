a108fd8e719212b2bd7b84e8da3cebf07eb4b89d
artmur1 <asmurchin@gmail.com>
2025-01-06 15:25:49 +0000

    answer2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,3 +15,18 @@ variable "vm_resources" {
     }
   }
 }
+
+variable "token" {
+  type        = string
+  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+}
+
+variable "cloud_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
